803356d8d59cb4be9a1649ec0a8ececaff0e6c19
alanpcao@gmail.com <alanpcao@gmail.com>
2020-04-21 11:06:34 -0700

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Alan Cao on 4/21/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
